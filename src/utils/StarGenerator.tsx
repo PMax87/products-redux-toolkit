835cb1f8d216c2dd1d474fac794b9a8a2b1f968c
PMax87 <noitulove@gmail.com>
2023-10-23 15:13:47 +0200

    Fixed Star Generator

--- src/utils/StarGenerator.tsx
+++ src/utils/StarGenerator.tsx
@@ -1,14 +1,16 @@
 import { BsStarFill, BsStarHalf, BsStar } from "react-icons/bs";
 
 export const starGenerator = (rating: number) => {
+  console.log(rating);
+
   const star = [];
   for (let i = 1; i <= 5; i++) {
     if (i <= rating) {
-      star.push(<BsStarFill key={i} />);
-    } else if (i - 0.5 === rating) {
-      star.push(<BsStarHalf key={i} />);
+      star.push(<BsStarFill key={i} className="text-yellow-400" />);
+    } else if (i - 0.5 <= rating) {
+      star.push(<BsStarHalf key={i} className="text-yellow-400" />);
     } else {
-      star.push(<BsStar key={i} />);
+      star.push(<BsStar key={i} className="text-yellow-400" />);
     }
   }
   return star;
